0d1200051f57db23c4fdc421fc2b6c1838d70221
msammons82 <87323843+msammons82@users.noreply.github.com>
2022-01-30 09:20:05 -0500

    Add files via upload

---
+++ birddataTime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 3
+# Time and Date bird data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+
+birddata = pd.read_csv("C:/Users/micha/Documents/Python Learning/bird_tracking.csv")
+bird_names = pd.unique(birddata.bird_name)
+
+timestamps = []
+for k in range(len(birddata)):
+    timestamps.append(datetime.datetime.strptime(birddata.date_time.iloc[k][:-3], "%Y-%m-%d %H:%M:%S"))
+
+birddata["timestamp"] = pd.Series(timestamps, index = birddata.index)
+
+times = birddata.timestamp[birddata.bird_name == "Eric"]
+elapsed_time = [time-times[0] for time in times]
+
+plt.plot(np.array(elapsed_time)/datetime.timedelta(days=1))
+plt.xlabel(" Observation ")
+plt.ylabel(" Elapsed time (days) ")
+plt.show()
